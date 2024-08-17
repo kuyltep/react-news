41f9ea200edea4e479fc8be302fd60bd6e4cf488
Vlad Petlyuk <vlad10112005@inbox.ru>
2024-08-17 09:11:22 +0300

    add pagination component

--- src/api/apiNews.ts
+++ src/api/apiNews.ts
@@ -1,15 +1,20 @@
 import axios from "axios";
 import { ResponseI } from "./types/NewsI";
+import { ApiParamsI } from "./types/ApiParamsI";
 
 const BASE_URL = import.meta.env.VITE_NEWS_API_BASE_URL;
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 
-export const getNews = async (
-  endpoint: string
-): Promise<ResponseI | undefined> => {
+export const getNews = async ({
+  endpoint,
+  page_number = 1,
+  page_size = 10,
+}: ApiParamsI): Promise<ResponseI | undefined> => {
   try {
     const response = await axios.get(`${BASE_URL}${endpoint}`, {
       params: {
+        page_number,
+        page_size,
         apiKey: API_KEY,
       },
     });
--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -4,25 +4,34 @@ import { getNews } from "../../api/apiNews";
 import { NewsI } from "../../api/types/NewsI";
 import NewsList from "../../components/News/NewsList/NewsList";
 import Skeleton from "../../components/Skeleton/Skeleton";
+import Pagination from "../../components/Pagination/Pagination";
 
 function Main() {
   const [news, setNews] = useState<NewsI[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(true);
-  useEffect(() => {
-    const fetchNews = async () => {
-      try {
-        setIsLoading(true);
-        const response = await getNews("latest-news");
-        if (response && response.news.length) {
-          setNews(response.news);
-          setIsLoading(false);
-        }
-      } catch (error) {
-        console.log(error);
+
+  const [pageNumber, setPageNumber] = useState(1);
+  const totalPages = 10;
+  const [pageSize, setPageSize] = useState(10);
+  const fetchNews = async () => {
+    try {
+      setIsLoading(true);
+      const response = await getNews({
+        endpoint: "search",
+        page_number: pageNumber,
+        page_size: pageSize,
+      });
+      if (response && response.news.length) {
+        setNews(response.news);
+        setIsLoading(false);
       }
-    };
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
     fetchNews();
-  }, []);
+  }, [pageNumber]);
   return (
     <div>
       {news.length > 0 && !isLoading ? (
@@ -40,6 +49,11 @@ function Main() {
       ) : (
         <Skeleton count={10} type="item" />
       )}
+      <Pagination
+        activePage={pageNumber}
+        totalPages={totalPages}
+        setActivePage={setPageNumber}
+      />
     </div>
   );
 }
