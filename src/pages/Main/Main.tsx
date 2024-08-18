d81cff2a96f4d89d0cd5e8b32459bb52c6057860
Vlad Petlyuk <vlad10112005@inbox.ru>
2024-08-18 15:33:35 +0300

    add searching logic with debounce

--- src/pages/Main/Main.tsx
+++ src/pages/Main/Main.tsx
@@ -6,6 +6,8 @@ import NewsList from "../../components/News/NewsList/NewsList";
 import Skeleton from "../../components/Skeleton/Skeleton";
 import Pagination from "../../components/Pagination/Pagination";
 import Categories from "../../components/Categories/Categories";
+import Search from "../../components/Search/Search";
+import useDebounce from "../../helpers/hooks/useDebounce";
 
 function Main() {
   const [news, setNews] = useState<NewsI[]>([]);
@@ -14,6 +16,9 @@ function Main() {
   const [pageNumber, setPageNumber] = useState<number>(1);
   const totalPages = 10;
   const [pageSize, setPageSize] = useState<number>(10);
+
+  const [keywords, setKeywords] = useState<string>("");
+  const debouncedKeywords = useDebounce(keywords, 1500);
 
   const [category, setCategory] = useState<string>("");
   const fetchNews = async () => {
@@ -24,6 +29,7 @@ function Main() {
         page_number: pageNumber,
         page_size: pageSize,
         category,
+        keywords: debouncedKeywords,
       });
       if (response && response.news.length) {
         setNews(response.news);
@@ -35,10 +41,11 @@ function Main() {
   };
   useEffect(() => {
     fetchNews();
-  }, [pageNumber, category]);
+  }, [pageNumber, category, debouncedKeywords]);
   return (
     <div>
       <Categories activeCategory={category} setActiveCategory={setCategory} />
+      <Search keywords={keywords} setKeywords={setKeywords} />
       {news.length > 0 && !isLoading ? (
         <NewsBanner
           author={news[0].author}
