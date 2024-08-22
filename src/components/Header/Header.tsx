fdd5076e19a8423b80d3e71b277cfa4295c78136
Vlad Petlyuk <vlad10112005@inbox.ru>
2024-08-22 14:40:46 +0300

    add new feature - theme context

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,29 @@
 import { formatDate } from "../../helpers/formatDate";
 import classes from "./styles.module.css";
+import { useTheme } from "../../context/theme";
+
 function Header() {
+  const { isDark, toogleTheme } = useTheme();
   return (
-    <header className={classes.header}>
-      <h1 className={classes.title}>React News</h1>
-      <p className={classes.time}>{formatDate(new Date())}</p>
+    <header
+      className={`${classes.header} ${isDark ? classes.dark : classes.light}`}
+    >
+      <div>
+        <h1 className={classes.title}>React News</h1>
+        <p className={classes.time}>{formatDate(new Date())}</p>
+      </div>
+      <div>
+        <button className={classes.theme} onClick={() => toogleTheme()}>
+          <img
+            src={
+              isDark
+                ? "src/assets/theme/dark.png"
+                : "src/assets/theme/light.png"
+            }
+            alt=""
+          />
+        </button>
+      </div>
     </header>
   );
 }
--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,8 +1,10 @@
 import { SearchPropsI } from "./types/SearchPropsI";
 import styles from "./styles.module.css";
+import { useTheme } from "../../context/theme";
 function Search({ keywords, setKeywords }: SearchPropsI) {
+  const { isDark } = useTheme();
   return (
-    <div className={styles.search}>
+    <div className={`${styles.search} ${isDark ? styles.dark : styles.light}`}>
       <input
         className={styles.input}
         type="text"
